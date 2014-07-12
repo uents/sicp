468c0505a8b334378f8a6c68b80ef6df9d076c70
uents <uents@ymail.com>
2014-07-12 17:04:31 +0900

    solved ex. 2.64

--- ch2.3.3.scm
+++ ch2.3.3.scm
@@ -143,7 +143,8 @@
                     (adjoin-set x (right-branch set))))))
 
   
-;; EXERCISE 2.63
+;;; ex. 2.63
+
 (define (tree->list-1 tree)
   (if (null? tree)
       '()
@@ -170,18 +171,18 @@
 (define (partial-tree elts n)
   (if (= n 0)
       (cons '() elts)
-      (let ((left-size (quotient (- n 1) 2)))
-        (let ((left-result (partial-tree elts left-size)))
-          (let ((left-tree (car left-result))
-                (non-left-elts (cdr left-result))
-                (right-size (- n (+ left-size 1))))
-            (let ((this-entry (car non-left-elts))
-                  (right-result (partial-tree (cdr non-left-elts)
-                                              right-size)))
-              (let ((right-tree (car right-result))
-                    (remaining-elts (cdr right-result)))
-                (cons (make-tree this-entry left-tree right-tree)
-                      remaining-elts))))))))
+      (let* ((left-size (quotient (- n 1) 2))
+			 (left-result (partial-tree elts left-size))
+			 (left-tree (car left-result))
+			 (non-left-elts (cdr left-result))
+			 (right-size (- n (+ left-size 1)))
+			 (this-entry (car non-left-elts))
+			 (right-result (partial-tree (cdr non-left-elts)
+										 right-size))
+			 (right-tree (car right-result))
+			 (remaining-elts (cdr right-result)))
+		(cons (make-tree this-entry left-tree right-tree)
+			  remaining-elts))))
 
 
 ;;;; information retrieval
