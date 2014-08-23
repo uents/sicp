d18b6ae53ec1c85f86bb94b043403b82a340064b
uents <uents@ymail.com>
2014-08-24 08:23:28 +0900

    make-account手続きをJavaScriptで実装

---
+++ ch3.1-make-account.js
@@ -0,0 +1,17 @@
+
+var Account = function(balance) {
+	this.withdraw = function(amount) {
+		if (balance >= amount) {
+			balance = balance - amount;
+			return balance;
+		} else {
+			return 'Insufficient funds';
+		}
+	};
+
+	this.deposit = function(amount) {
+		balance = balance + amount;
+		return balance;
+	};
+};
+
