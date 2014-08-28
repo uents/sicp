dcfa0b22cb6e496b632e85774927cf8015afc884
uents <uents@ymail.com>
2014-08-29 02:21:13 +0900

    問題 3.25 (やっと解けた)

--- ch3.3.3.scm
+++ ch3.3.3.scm
@@ -26,93 +26,80 @@
 (require r5rs)
 
 
-;;; 1次元テーブル
-(define (lookup key table)
-  (let ((record (assoc key (cdr table))))
-    (if record
-        (cdr record)
-        false)))
-
-(define (assoc key records)
-  (cond ((null? records) false)
-        ((equal? key (caar records)) (car records))
-        (else (assoc key (cdr records)))))
-
-(define (insert! key value table)
-  (let ((record (assoc key (cdr table))))
-    (if record
-        (set-cdr! record value)
-        (set-cdr! table
-                  (cons (cons key value) (cdr table)))))
-  'ok)
-
-(define (make-table)
-  (list '*table*))
-
-
-;;; 2次元テーブル
-(define (lookup key-1 key-2 table)
-  (let ((subtable (assoc key-1 (cdr table))))
-	(if subtable
-		(let ((record (assoc key-2 (cdr subtable))))
-		  (if (record
-			   (cdr record))
-			  false))
-		false)))
-
-(define (insert key-1 key-2 value table)
-  (let ((subtable (assoc key-1 (cdr table))))
-	(if subtable
-		(let ((record (assoc key-2 (cdr subtable))))
-		  (if record
-			  (set-cdr! record value)
-			  (set-cdr! subtable
-						(cons (cons key-2 value) (cdr subtable)))))
-		(set-cdr! table
-				  (cons (list key-1 (cons key-2 value))
-						(cdr table)))))
-  'ok)
-
-
-;;; ローカルなテーブル
-(define (make-table)
-  (let ((local-table (list '*table*)))
-	(define (lookup key-1 key-2)
-	  (let ((subtable (assoc key-1 (cdr local-table))))
-		(if subtable
-			(let ((record (assoc key-2 (cdr subtable))))
-			  (if (record
-				   (cdr record))
-				  false))
-			false)))
-	(define (insert key-1 key-2 value)
-	  (let ((subtable (assoc key-1 (cdr local-table))))
-		(if subtable
-			(let ((record (assoc key-2 (cdr subtable))))
-			  (if record
-				  (set-cdr! record value)
-				  (set-cdr! subtable
-							(cons (cons key-2 value) (cdr subtable)))))
-			(set-cdr! local-table
-					  (cons (list key-1 (cons key-2 value))
-							(cdr local-table)))))
-	  'ok)
-	(define (dispatch m)
-	  (cond ((eq? m 'lookup-proc) lookup)
-			((eq? m 'insert-proc!) insert!)
-			(else (error "Unknown operation -- TABLE" m))))
-	disptach))
-
-(define operation-table (make-table))
-(define get (operation-table 'lookup-proc))
-(define put (operation-table 'insert-proc!))
-
-
 ;;; ex 3.24
 
 
 ;;; ex 3.25
 
+(define (make-record key value)
+  (cons key value))
+(define (key-record record)
+  (car record))
+(define (value-record record)
+  (cdr record))
+(define (key-record! key record)
+  (set-car! record key))
+(define (value-record! value record)
+  (set-cdr! record value))
+
+(define (assoc key records)
+  (cond ((null? records) false)
+        ((equal? key (key-record (car records))) (car records))
+        (else (assoc key (cdr records)))))
+
+(define (adjoin! key-list value records)
+  (define (new-record key-list value)
+	(if (null? (cdr key-list))
+		(make-record (car key-list) value)
+		(list (car key-list)
+			  (new-record (cdr key-list) value))))
+  (set-cdr! records
+			(cons (new-record key-list value) (cdr records))))
+
+(define (make-table)
+  (let ((the-table (list '*table*)))
+	(define (lookup key-list)
+	  (define (iter key-list table)
+		(if (null? key-list)
+			false
+			(let ((record (assoc (car key-list) (cdr table))))
+			  (if record
+				  (if (null? (cdr key-list))
+					  (value-record record)
+					  (iter (cdr key-list) record))
+				  false))))
+	  (iter key-list the-table))
+	(define (insert! key-list value)
+	  (define (iter key-list table)
+		(if (null? key-list)
+			false
+			(let ((record (assoc (car key-list) (cdr table))))
+			  (if record
+				  (if (null? (cdr key-list))
+					  (value-record! value record)
+					  (iter (cdr key-list) record))
+				  (adjoin! key-list value table)))))
+	  (iter key-list the-table))
+
+	(define (dispatch m)
+	  (cond ((eq? m 'lookup-proc) lookup)
+			((eq? m 'insert-proc!) insert!)
+			(else (error "Unknown operation -- TABLE" m))))
+	dispatch))
+
+; racket@> (define tbl (make-table))
+; racket@> ((tbl 'insert-proc!) (list 'foo 'bar) 1)
+; racket@> ((tbl 'insert-proc!) (list 'foo 'baz) 2)
+; racket@> ((tbl 'insert-proc!) (list 'foo 'qux) 3)
+; racket@> ((tbl 'insert-proc!) (list 'bar 'baz) 11)
+; racket@> ((tbl 'insert-proc!) (list 'bar 'qux) 12)
+; 
+; racket@> ((tbl 'lookup-proc) (list 'foo 'baz))
+; 2
+; racket@> ((tbl 'lookup-proc) (list 'bar 'foo))
+; #f
+; racket@> ((tbl 'lookup-proc) (list 'bar 'baz))
+; 11
 
 ;;; ex 3.26
 
