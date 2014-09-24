64b89e6efecf5f6e0f0b654ca18847c6c1a232ab
uents <uents@ymail.com>
2014-09-25 01:12:26 +0900

    added append!

--- misc.scm
+++ misc.scm
@@ -6,3 +6,24 @@
 
 (define (square x) (* x x))
 
+(define (my-last-pair x)
+  (if (null? (cdr x))
+      x
+      (my-last-pair (cdr x))))
+
+(define (append! x y)
+  (if (pair? x)
+	  (begin
+		(set-cdr! (my-last-pair x) y)
+		x)
+	  (error "Object is not pair" x)))
+  
+;; 引数xがnilの場合に対応したくても
+;; 以下の実装では値渡しとなるため期待通りには動かない
+;; (define (append! x y)
+;;   (cond ((null? x) (set! x y))
+;; 		((pair? x) (set-cdr! (my-last-pair x) y))
+;; 		(else (error "Object is atom" x)))
+;;   x)
+
+
