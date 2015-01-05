298b04985e73db940c7a9417488ddded66210ac8
uents <uents@ymail.com>
2015-01-05 14:15:49 +0900

    solved ex 3.33-35

--- ch3.3.5.scm
+++ ch3.3.5.scm
@@ -212,3 +212,143 @@
   (set-value! connector value me)
   me)
 
+;;; multiplier
+(define (multiplier m1 m2 product)
+  (define (process-new-value)
+    (cond ((or (and (has-value? m1) (= (get-value m1) 0))
+               (and (has-value? m2) (= (get-value m2) 0)))
+           (set-value! product 0 me))
+          ((and (has-value? m1) (has-value? m2))
+           (set-value! product
+                       (* (get-value m1) (get-value m2))
+                       me))
+          ((and (has-value? product) (has-value? m1))
+           (set-value! m2
+                       (/ (get-value product) (get-value m1))
+                       me))
+          ((and (has-value? product) (has-value? m2))
+           (set-value! m1
+                       (/ (get-value product) (get-value m2))
+                       me))))
+  (define (process-forget-value)
+    (forget-value! product me)
+    (forget-value! m1 me)
+    (forget-value! m2 me)
+    (process-new-value))
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value)
+           (process-new-value))
+          ((eq? request 'I-lost-my-value)
+           (process-forget-value))
+          (else
+           (error "Unknown request -- MULTIPLIER" request))))
+  (connect m1 me)
+  (connect m2 me)
+  (connect product me)
+  me)
+
+
+
+;;;; -----------------------------------
+;;;; excercise
+;;;; -----------------------------------
+
+;;;; ex 3.33
+(define (averager a b c)
+  (let ((u (make-connector))
+		(v (make-connector))
+		(w (make-connector)))
+	(adder a b u)
+	(multiplier c v u)
+	(constant 2 v)
+	'ok))
+
+
+;;; test
+;; racket@> (define a (make-connector))
+;; (define b (make-connector))
+;; (define c (make-connector))
+;; (probe "a" a)
+;; (probe "b" b)
+;; (probe "c" c)
+;; (averager a b c)
+;; 'ok
+;; racket@> (set-value! a 3 'user)
+;; Probe: a = 3
+;; 'done
+;; racket@> (set-value! b 5 'user)
+;; Probe: c = 4
+;; Probe: b = 5
+;; 'done
+;; racket@> (forget-value! b 'user)
+;; Probe: c = ?
+;; Probe: b = ?
+;; 'done
+;; racket@> (set-value! c 1 'user)
+;; Probe: b = -1
+;; Probe: c = 1
+;; 'done
+
+
+;;;; ex 3.34
+; bに値を設定しても２つのaの値が決定されるわけではないため動作しない
+
+;;;; ex 3.35
+(define (squarer a b)
+  (define (process-new-value)
+	(if (has-value? b)
+		(if (< (get-value b) 0)
+			(error "square less than 0 --- SQUARER" (get-value b))
+			(set-value! a (sqrt (get-value b)) me))         ; <代替部1>
+		(set-value! b (* (get-value a) (get-value a)) me))) ; <代替部2>
+  (define (process-forget-value)
+	(forget-value! a me)  ; <本体1>
+	(forget-value! b me))
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value)  ; <本体2>
+           (process-new-value))
+          ((eq? request 'I-lost-my-value) 
+           (process-forget-value))
+          (else 
+           (error "Unknown request -- SQUARER" request))))
+  (connect a me)   ; <定義の残り>
+  (connect b me)
+  me)
+
+;;; test
+;; racket@> (define x (make-connector))
+;; (define y (make-connector))
+;; (probe "x" x)
+;; (probe "y" y)
+;; (squarer x y)
+;; racket@> (set-value! x 3 'user)
+;; Probe: y = 9
+;; Probe: x = 3
+;; 'done
+;; racket@> (forget-value! x 'user)
+;; Probe: y = ?
+;; Probe: x = ?
+;; 'done
+;; racket@> (set-value! y 3 'user)
+;; Probe: x = 1.7320508075688772
+;; Probe: y = 3
+;; 'done
+
+
+;;;; ex 3.36 
+;; 略
+
+;;;; ex 3.37
+(define (celsius-fahrenheit-converter x)
+  (c+ (c* (c/ (cv 9) (cv 5))
+          x)
+      (cv 32)))
+
+;: (define C (make-connector))
+;: (define F (celsius-fahrenheit-converter C))
+
+(define (c+ x y)
+  (let ((z (make-connector)))
+    (adder x y z)
+    z))
+
