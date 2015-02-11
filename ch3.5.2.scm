0ae114f9a31297e574614c0b3167892d1e8bad49
uents <uents@ymail.com>
2015-02-11 09:39:06 +0900

    solved ex 3.53-57

--- ch3.5.2.scm
+++ ch3.5.2.scm
@@ -25,7 +25,6 @@
 
 
 
-
 (define ones
   (stream-cons 1 ones))
 
@@ -64,12 +63,12 @@
 
 ;; racket@> (map (lambda (x) (stream-ref fibs x))
 ;; 			  (enumerate-interval 0 10))
-;; '(0 1 1 2 3 5 8 13 21 34 55)
+;; => '(0 1 1 2 3 5 8 13 21 34 55)
 
 (define (integers-starting-from n)
   (stream-cons n (integers-starting-from (+ n 1))))
 
-;; prime? を使うためにロード
+;; for using prime?
 (require math/number-theory)
 
 (define primes
@@ -79,10 +78,69 @@
 
 
 ;;; ex 3.53
-(define s (stream-cons 1 (add-stream s s)))
+(define s (stream-cons 1 (add-streams s s)))
+
+;; racket@> (map (lambda (x) (stream-ref s x))
+;;			  (enumerate-interval 0 10))
+;; => '(1 2 4 8 16 32 64 128 256 512 1024)
 
 
 ;;; ex 3.54
 
-(define (mul-stream s1 s2)
-  (stream-cons 
+;; see mul-streams in streams.scm
+
+(define factorials
+  (stream-cons 1 (mul-streams integers factorials)))
+
+;; racket@> (map (lambda (i) (stream-ref factorials i))
+;; 			  (enumerate-interval 0 10))
+;; '(1 1 2 6 24 120 720 5040 40320 362880 3628800)
+
+
+;;; ex 3.55
+(define (practical-sum s)
+  (stream-cons (stream-car s)
+			   (add-streams (practical-sum s) (stream-cdr s))))
+
+;; racket@> (map (lambda (i) (stream-ref (practical-sum integers) i))
+;; 			  (enumerate-interval 0 10))
+;; => '(1 3 6 10 15 21 28 36 45 55 66)
+
+
+;;; ex 3.56
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+         (let ((s1car (stream-car s1))
+               (s2car (stream-car s2)))
+           (cond ((< s1car s2car)
+                  (stream-cons s1car (merge (stream-cdr s1) s2)))
+                 ((> s1car s2car)
+                  (stream-cons s2car (merge s1 (stream-cdr s2))))
+                 (else
+                  (stream-cons s1car
+                               (merge (stream-cdr s1)
+                                      (stream-cdr s2)))))))))
+
+(define S
+  (stream-cons 1
+			   (merge (scale-stream S 2)
+					  (merge (scale-stream S 3) (scale-stream S 5)))))
+
+;; racket@> (map (lambda (i) (stream-ref S i))
+;; 			  (enumerate-interval 0 20))
+;; => '(1 2 3 4 5 6 8 9 10 12 15 16 18 20 24 25 27 30 32 36 40)
+
+
+;;; ex 3.57
+
+;; expandという名前はRacketの組み込みマクロと
+;; 重複するので、別の名前で実装
+
+(define (my-expand num den radix)
+  (stream-cons
+   (quotient (* num radix) den)
+   (my-expand (remainder (* num radix) den) den radix)))
+
+ 
