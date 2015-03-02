93c57158bfb09e6d6559c4324f3f5a8f9166bc44
uents <uents@ymail.com>
2015-03-03 08:30:28 +0900

    implementing

--- ch4-evaluator.rb
+++ ch4-evaluator.rb
@@ -46,3 +46,108 @@ def _apply(procedure, arguments)
   end
 end
 
+def cons(x, y)
+  [a] + b
+end
+
+def car(list)
+  list[0]
+end
+
+def cdr(list)
+  list[1..-1]
+end
+
+def pair?(list)
+  list.is_a?(Array) && list.length >= 2
+end
+
+# 手続きの引数
+def list_of_values(exps, env)
+  if no_operands?(exps)
+    nil
+  else
+    cons(_eval(first_operand(exps), env),
+         list_of_values(rest_operand(exps), env))
+  end
+end
+
+# 条件文
+def eval_if(exp, env)
+  if true?(_eval(if_predicate(exp), env))
+    _eval(if_consequent(exp), env)
+  else
+    _eval(if_alternative(exp), env)
+  end
+end
+
+# シーケンス
+def eval_sequence(exps, env)
+  if last_exp?(exps)
+    _eval(first_exp(exps), env)
+  else
+    _eval(first_exp(exps), env)
+    eval_sequence(rest_exps(exps), env)
+  end
+end
+
+# 代入
+def eval_assignment(exp, env)
+  var = assignment_variable(exp)
+  value = _eval(assignment_value(exp), env)
+  set_variable_value!(var, value, env)
+end
+
+# 定義
+def eval_definition(exp, env)
+  var = definition_variable(exp)
+  value = _eval(definition_value(exp), env)
+  definition_variable!(var, value, env)
+end
+
+# 数値/文字列
+def self_evaluating?(exp)
+  if number?(exp)
+    true
+  elsif string?(exp)
+    true
+  else
+    false
+  end
+end
+
+def number?(exp)
+  exp.is_a?(Numeric)
+end
+
+def string?(exp)
+  exp.is_a?(String)
+end
+
+# 変数
+def variable?(exp)
+  symbol?(exp)
+end
+
+def symbol?(exp)
+  exp.is_a?(Symbol)
+end
+
+# クオート
+def quoted?(exp)
+  tagged_list?(exp, :quote)
+end
+
+def text_of_quotation(exp)
+  car(cdr(exp))
+end
+
+def tagged_list?(exp, tag)
+  if pair?(exp)
+    car(exp) == tag
+  else
+    false
+  end
+end
+
+# 代入
