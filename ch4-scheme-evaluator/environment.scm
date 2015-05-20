6f0c1f5f7147cb5a9cd49100763ec57d1790a9f4
uents <uents@ymail.com>
2015-05-21 06:35:54 +0900

    implement environment

--- ch4-scheme-evaluator/environment.scm
+++ ch4-scheme-evaluator/environment.scm
@@ -1,9 +1,43 @@
 
 (define (setup-environment)
-  nil) ;; @@@TODO
+  (let ((frame (make-hash)))
+	(define-variable! 'true true)
+	(define-variable! 'false false)
+	(list frame)))
 
 (define (extend-environment vars vals env)
-  nil) ;; @@@TODO
-(define (lookup-variable-value var env)
-  nil) ;; @@@TODO
+  (with-handlers
+	  ([exn:fail? (lambda (exn)
+					(error "extend-environment: arguments error:"
+						   vars vals))])
+	(cons (make-frame vars vals) env)))
 
+(define (make-frame vars vals)
+  (let ((frame (make-hash)))
+	(map (lambda (var val) (hash-set! frame var val))
+		 vars vals)
+	frame))
+
+(define (lookup-variable-value var env)
+  (define (traverse env)
+	(if (eq? env nil)
+		(error "lookup-variable-value: unbound variable:" var)
+		(let ((frame (car env)))
+		  (if (hash-has-key? frame var)
+			  (hash-ref frame var)
+			  (traverse (cdr env))))))
+  (traverse env))
+
+(define (define-variable! var val env)
+  (let ((frame (car env)))
+	(hash-set! frame var val)))
+
+(define (set-variable-value! var val env)
+  (define (traverse env)
+	(if (eq? env nil)
+		(error "set-variable-value!: unbound variable:" var)
+		(let ((frame (car env)))
+		  (if (hash-has-key? frame var)
+			  (hash-set! frame var val)
+			  (traverse (cdr env))))))
+  (traverse env))
