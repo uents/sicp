44cbd2caced16198672d63f1ce6edb483db11012
uents <uents@ymail.com>
2015-05-22 06:32:49 +0900

    implement assginment, definition and condition (but not test)

--- ch4-scheme-evaluator/special-forms.scm
+++ ch4-scheme-evaluator/special-forms.scm
@@ -6,18 +6,30 @@
 
 ;;; assignment
 (define (make-assginment exp)
-  (lambda (env)
-	exp)) ;; @@@TODO
+  (let ((var (cadr exp))
+		(val (parse (caddr exp))))
+	(lambda (env)
+	  (set-variable-value! var val env))))
 
 ;;; definition
 (define (make-definition exp)
-  (lambda (env)
-	exp)) ;; @@@TODO
+  (let ((var (if (symbol? (cadr exp))
+				 (cadr exp) (caadr exp)))
+		(val (parse (if (symbol? (cadr exp))
+						(caddr exp)
+						(make-lambda (cdadr exp) (cddr exp))))))
+	(lambda (env)
+	  (define-variable! var val env))))
 
 ;;; condition
 (define (make-if exp)
-  (lambda (env)
-	exp)) ;; @@@TODO
+  (let ((predicate (cadr exp))
+		(consequent (caddr exp))
+		(alternative (cadddr exp)))
+	(lambda (env)
+	  (if (true? (eval-proc predicate env))
+		  (eval-proc consequent env)
+		  (eval-proc alternative env)))))
 
 ;;; lambda
 (define (make-lambda exp)
