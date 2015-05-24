90ecb3ee8ec5a7a78b36c6d5bfa98976d694a97f
uents <uents@ymail.com>
2015-05-24 23:03:12 +0900

    rename procedure name: parse-* -> analyze-*

--- ch4-scheme-evaluator/special-forms.scm
+++ ch4-scheme-evaluator/special-forms.scm
@@ -1,28 +1,28 @@
 
 ;;; quotation
-(define (make-quotation exp)
+(define (analyze-quoted exp)
   (lambda (env)
 	exp)) ;; @@@TODO
 
 ;;; assignment
-(define (make-assginment exp)
+(define (analyze-assginment exp)
   (let ((var (cadr exp))
-		(val (parse (caddr exp))))
+		(val (analyze (caddr exp))))
 	(lambda (env)
 	  (set-variable-value! var val env))))
 
 ;;; definition
-(define (make-definition exp)
+(define (analyze-definition exp)
   (let ((var (if (symbol? (cadr exp))
 				 (cadr exp) (caadr exp)))
-		(val (parse (if (symbol? (cadr exp))
-						(caddr exp)
-						(make-lambda (cdadr exp) (cddr exp))))))
+		(val (analyze (if (symbol? (cadr exp))
+						  (caddr exp)
+						  (analyze-lambda (cdadr exp) (cddr exp))))))
 	(lambda (env)
 	  (define-variable! var val env))))
 
 ;;; condition
-(define (make-if exp)
+(define (analyze-if exp)
   (let ((predicate (cadr exp))
 		(consequent (caddr exp))
 		(alternative (cadddr exp)))
@@ -32,14 +32,14 @@
 		  (eval-proc alternative env)))))
 
 ;;; lambda
-(define (make-lambda exp)
-  (let ((params (map parse (cadr exp)))
-		(body   (map parse (cddr exp))))
+(define (analyze-lambda exp)
+  (let ((params (map analyze (cadr exp)))
+		(body (map analyze (cddr exp))))
 	(lambda (env)
-	  (make-procedure params body env))))
+	  (analyze-procedure params body env))))
 
 ;;; sequence
-(define (make-sequence exp)
+(define (analyze-sequence exp)
   (lambda (env)
 	exp)) ;; @@@TODO
 
