f4daa96c3730ad74446e32b62f10b971096bcb6c
uents <uents@ymail.com>
2015-05-24 23:52:38 +0900

    solve ex 4.23

--- ch4-scheme-evaluator/special-forms.scm
+++ ch4-scheme-evaluator/special-forms.scm
@@ -40,6 +40,28 @@
 
 ;;; sequence
 (define (analyze-sequence exp)
-  (lambda (env)
-	exp)) ;; @@@TODO
+  (analyze-sequence-original exp))
+;  (analyze-sequence-by-alyssa exp))
 
+(define (analyze-sequence-original exps)
+  (define (sequentially proc1 proc2)
+    (lambda (env) (proc1 env) (proc2 env)))
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+        first-proc
+        (loop (sequentially first-proc (car rest-procs))
+              (cdr rest-procs))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+        (error "Empty sequence -- ANALYZE")
+		(loop (car procs) (cdr procs)))))
+
+(define (analyze-sequence-by-alyssa exps)
+  (define (execute-sequence procs env)
+    (cond ((null? (cdr procs)) ((car procs) env))
+          (else ((car procs) env)
+                (execute-sequence (cdr procs) env))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+        (error "Empty sequence -- ANALYZE")
+		(lambda (env) (execute-sequence procs env)))))
