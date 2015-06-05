3d9e4b6cfb288a85bad6cb3eab1b4ada25ade85c
uents <uents@ymail.com>
2015-06-05 21:58:49 +0900

    update

--- ch4.3.scm
+++ ch4.3.scm
@@ -1,15 +1,34 @@
 
+(define nil '())
+
+(load "amb.scm")
+
+;;; SICP本文でのrequireはnecessaryという名前で定義
+
+(define (necessary p)
+  (if (not p) (amb) false))
 
 (define (an-element-of items)
-  (require (not (null? items)))
-  (amb (car items) (an-element-of (cdr items))))
+  (necessary (not (null? items)))
+  (apply amb items))
 
-(define (product list1 list2)
+
+;;;; 4.3.1 ambと検索
+
+(require math/number-theory)
+
+(define (prime-sum-pair list1 list2)
   (let ((a (an-element-of list1))
         (b (an-element-of list2)))
+	(necessary (prime? (+ a b)))
     (list a b)))
 
-(define nil '())
+;(define (an-integer-starting-from n)
+;  (amb n (an-integer-starting-from (+ n 1))))
+
+
+
+;;; ex 4.35
 
 (define (enumerate-interval low high)
   (if (> low high)
@@ -18,3 +37,4 @@
 
 (define (an-integer-between low high)
   (an-element-of (enumerate-interval low high)))
+
