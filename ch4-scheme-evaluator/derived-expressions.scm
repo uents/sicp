27f8ca3a173dc26ccbee17b418af84c40223450a
uents <uents@ymail.com>
2015-06-07 17:17:32 +0900

    fix typo

--- ch4-scheme-evaluator/derived-expressions.scm
+++ ch4-scheme-evaluator/derived-expressions.scm
@@ -1,9 +1,9 @@
 
 (define (let->combination exp)
   (let ((variables (map car (cadr exp)))
-		(expressios (map cadr (cadr exp))))
+		(expressions (map cadr (cadr exp))))
 		(body (cddr exp)))
-  (cons (cons 'lambda (cons variable body))
+  (cons (cons 'lambda (cons variables body))
 		expressions))
 
 
