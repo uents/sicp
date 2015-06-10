d6e4d6cbd26c40527248d1c917445b23b8d00090
uents <uents@ymail.com>
2015-06-10 16:00:08 +0900

    update

--- ch4.2.scm
+++ ch4.2.scm
@@ -72,6 +72,12 @@
 
 ;;; ex 4.30
 
+;; original
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps) (eval (first-exp exps) env))
+        (else (eval (first-exp exps) env)
+              (eval-sequence (rest-exps exps) env))))
+
 ;; Cy's proposal
 (define (eval-sequence exps env)
   (cond ((last-exp? exps) (eval (first-exp exps) env))
@@ -85,12 +91,24 @@
 	  (begin (proc (car items))
 			 (for-each proc (cdr items)))))
 
-
 (for-each (lambda (x) (newline) (display x))
 				  (list 57 321 88))
 
 ;; => 57
 ;; => 321
-;; => 88				   
+;; => 88
 
 
+;; b.
+(define (p1 x)
+  (set! x (cons x '(2)))
+  x)
+
+(define (p2 x)
+  (define (p e)
+	e
+	x)
+  (p (set! x (cons x '(2)))))
+
+;; (p1 1) => ???
+;; (p2 1) => ???
