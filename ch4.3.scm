8a787df3115073bc55ec41afb8432aaa403fa8df
uents <uents@ymail.com>
2015-07-03 07:05:03 +0900

    revise

--- ch4.3.scm
+++ ch4.3.scm
@@ -1,15 +1,22 @@
 
-(define nil '())
-
+;;; call/ccで実装したambオペレータ
 (load "amb.scm")
 
 ;;; SICP本文でのrequireはreqという名前で定義
-
 (define (req p)
   (if (not p) (amb) false))
 
+;;; ambオペレータを再帰的に呼ぶだけに、引数を遅延オブジェクトとする
+;;; SICP本文のamb評価器は、遅延評価するため不要
 (define (an-element-of items)
-  (apply amb items))
+  (req (not (null? items)))
+  (amb (car items) (delay (an-element-of (cdr items)))))
+
+;;; ambオペレータは特殊形式ではなく手続きの場合、
+;;; applyを使って以下のようにもかける
+;; (define (an-element-of items)
+;;   (apply amb items))
+
 
 ;;;; 4.3.1 ambと検索
 
@@ -23,15 +30,15 @@
 
 ;;(prime-sum-pair '(1 3 5 8) '(20 35 110))
 
-;;(define (an-integer-starting-from n)
-;;  (amb n (an-integer-starting-from (+ n 1))))
+(define (an-integer-starting-from n)
+  (amb n (delay (an-integer-starting-from (+ n 1)))))
 
 
 ;;; ex 4.35
 
 (define (enumerate-interval low high)
   (if (> low high)
-	  nil
+	  '()
 	  (cons low (enumerate-interval (+ low 1) high))))
 
 (define (an-integer-between low high)
