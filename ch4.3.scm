b4c94a23044ba33a4e026107ebc1f37e31f2bec0
uents <uents@ymail.com>
2015-07-06 22:52:05 +0900

    solved ex 4.36-37

--- ch4.3.scm
+++ ch4.3.scm
@@ -44,3 +44,58 @@
 (define (an-integer-between low high)
   (an-element-of (enumerate-interval low high)))
 
+
+
+;;; ex 4.36
+
+(define (a-pythagorean-triple-between low high)
+  (let ((i (an-integer-between low high)))
+	(let ((j (an-integer-between i high)))
+	  (let ((k (an-integer-between j high)))
+		(req (= (+ (* i i) (* j j)) (* k k)))
+		(list i j k)))))
+
+(define (a-pythagorean-triple)
+  (let ((i (an-integer-starting-from 1)))
+	(let ((j (an-integer-starting-from 1)))
+	  (let ((k (an-integer-starting-from 1)))
+		(req (= (+ (* i i) (* j j)) (* k k)))
+		(list i j k)))))
+;; => kだけが増えていくので返ってこない
+
+(define (a-pythagorean-triple)
+  (let* ((k (an-integer-starting-from 1))
+		 (j (an-integer-between 1 k))
+		 (i (an-integer-between 1 j)))
+	(req (= (+ (* i i) (* j j)) (* k k)))
+	(list i j k)))
+
+
+;;; ex 4.37
+
+(define *try-count* 0)
+
+(define (req p)
+  (if (not p)
+	  (begin (set! *try-count* (+ *try-count* 1))
+			 (amb))
+	  false))
+
+(define (a-pythagorean-triple-between-ex low high)
+  (let ((i (an-integer-between low high))
+        (hsq (* high high)))
+    (let ((j (an-integer-between i high)))
+      (let ((ksq (+ (* i i) (* j j))))
+        (req (>= hsq ksq))
+        (let ((k (sqrt ksq)))
+          (req (integer? k))
+          (list i j k))))))
+
+;; racket@> (a-pythagorean-triple-between-ex 1 20)
+;; => '(3 4 5)
+;; racket@> (try-again)
+;; ...
+;; racket@> (try-again)
+;; => '(there are no more values)
+;; racket@> *try-count*
+;; => 225
