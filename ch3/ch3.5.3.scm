41c5fc9c38103c6ec7b3803cf793a2e704d24aa6
uents <uents@ymail.com>
2015-09-01 08:52:22 +0900

    solve ex 3.65 and try ex 3.66

--- ch3/ch3.5.3.scm
+++ ch3/ch3.5.3.scm
@@ -134,28 +134,178 @@ guess=1.4142135623746899
   (stream-limit (sqrt-stream x) tolerance))
 
 (define (stream-limit s tolerance)
-  (let ((s0 (stream-ref s 0))
-		(s1 (stream-ref s 1)))
-	(if (< (abs (- s0 s1)) tolerance)
-		s1
-		(stream-limit (stream-cdr s) tolerance))))
+  (define (iter s count)
+	(let ((s0 (stream-ref s 0))
+		  (s1 (stream-ref s 1)))
+	  (if (< (abs (- s0 s1)) tolerance)
+		  (cons s1 count)
+		  (iter (stream-cdr s) (+ count 1)))))
+  (iter s 0))
 
 #|
-racket@> (stream-limit (sqrt-stream 2) 0.01)
-=> 1.4142156862745097
-racket@> (stream-limit (sqrt-stream 2) 0.001)
-=> 1.4142135623746899
+racket@> (sqrt 2 0.01)
+=> '(1.4142156862745097 . 2)
+racket@> (stream-limit (sqrt-stream 2) 0.00001)
+=> '(1.4142135623746899 . 3)
+
 |#
 
 
 ;;; ex 3.65
 
+(define (ln2-sum n)
+  (cons-stream (/ 1.0 n)
+			   (stream-map - (ln2-sum (+ n 1)))))
+
+(define ln2-stream
+  (partial-sums (ln2-sum 1)))
+
+#|
+racket@> (log 2)
+0.6931471805599453
+
+racket@> (map (lambda (n)
+				(stream-ref ln2-stream n))
+			  (enumerate-interval 0 10))
+'(1.0
+  0.5
+  0.8333333333333333
+  0.5833333333333333
+  0.7833333333333332
+  0.6166666666666666
+  0.7595238095238095
+  0.6345238095238095
+  0.7456349206349207
+  0.6456349206349207
+  0.7365440115440116)
+
+racket@> (map (lambda (n)
+				(stream-ref (euler-transform ln2-stream) n))
+			  (enumerate-interval 0 10))
+'(0.7
+  0.6904761904761905
+  0.6944444444444444
+  0.6924242424242424
+  0.6935897435897436
+  0.6928571428571428
+  0.6933473389355742
+  0.6930033416875522
+  0.6932539682539683
+  0.6930657506744464
+  0.6932106782106783)
+
+racket@> (map (lambda (n)
+				(stream-ref (accelerated-sequence euler-transform ln2-stream) n))
+			  (enumerate-interval 0 10))
+'(1.0
+  0.7
+  0.6932773109243697
+  0.6931488693329254
+  0.6931471960735491
+  0.6931471806635636
+  0.6931471805604039
+  0.6931471805599445
+  0.6931471805599427
+  0.6931471805599454
+  +nan.0)
+|#
+
 
 ;;;; 対の無限ストリーム
+
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (stream-map (lambda (x) (list (stream-car s) x))
+                (stream-cdr t))
+    (pairs (stream-cdr s) (stream-cdr t)))))
+
+(define (stream-append s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (stream-append (stream-cdr s1) s2))))
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
+;; from ch 3.5.2
+(define ones (cons-stream 1 ones))
+(define integers (cons-stream 1 (add-streams ones integers)))
 
 
 ;;; ex 3.66
 
+#|
+racket@> (map (lambda (k)
+				(let ((p (stream-ref (pairs integers integers) k)))
+				  (cons k (list p))))
+			  (enumerate-interval 0 25))
+'((0 (1 1))
+  (1 (1 2))
+  (2 (2 2))
+  (3 (1 3))
+  (4 (2 3))
+  (5 (1 4))
+  (6 (3 3))
+  (7 (1 5))
+  (8 (2 4))
+  (9 (1 6))
+  (10 (3 4))
+  (11 (1 7))
+  (12 (2 5))
+  (13 (1 8))
+  (14 (4 4))
+  (15 (1 9))
+  (16 (2 6))
+  (17 (1 10))
+  (18 (3 5))
+  (19 (1 11))
+  (20 (2 7))
+  (21 (1 12))
+  (22 (4 5))
+  (23 (1 13))
+  (24 (2 8))
+  (25 (1 14)))
+
+racket@> (filter
+		  (lambda (x) (= (caadr x) 1))
+		  (map (lambda (k)
+				 (let ((p (stream-ref (pairs integers integers) k)))
+				   (cons k (list p))))
+			   (enumerate-interval 0 10)))
+'((0 (1 1)) (1 (1 2)) (3 (1 3)) (5 (1 4)) (7 (1 5)) (9 (1 6)))
+
+racket@> (filter
+		  (lambda (x) (= (caadr x) 2))
+		  (map (lambda (k)
+				 (let ((p (stream-ref (pairs integers integers) k)))
+				   (cons k (list p))))
+			   (enumerate-interval 0 20)))
+'((2 (2 2)) (4 (2 3)) (8 (2 4)) (12 (2 5)) (16 (2 6)) (20 (2 7)))
+
+racket@> (filter
+		  (lambda (x) (= (caadr x) 3))
+		  (map (lambda (k)
+				 (let ((p (stream-ref (pairs integers integers) k)))
+				   (cons k (list p))))
+			   (enumerate-interval 0 30)))
+'((6 (3 3)) (10 (3 4)) (18 (3 5)) (26 (3 6)))
+
+racket@> (filter
+		  (lambda (x) (= (caadr x) 4))
+		  (map (lambda (k)
+				 (let ((p (stream-ref (pairs integers integers) k)))
+				   (cons k (list p))))
+			   (enumerate-interval 0 60)))
+'((14 (4 4)) (22 (4 5)) (38 (4 6)) (54 (4 7)))
+|#
+
+a
 ;;; ex 3.67
 
 ;;; ex 3.68
