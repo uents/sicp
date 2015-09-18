174f9998c9c428a26280d42ef8c3d9d24cf42d35
uents <uents@ymail.com>
2015-09-18 09:02:18 +0900

    ch4.4-query-system: revise

--- ch4.4-query-system/ch4-query.scm
+++ ch4.4-query-system/ch4-query.scm
@@ -21,18 +21,6 @@
 (define input-prompt ";;; Query input:")
 (define output-prompt ";;; Query results:")
 
-(define (instantiate exp frame unbound-var-handler)
-  (define (copy exp)
-    (cond ((var? exp)
-           (let ((binding (binding-in-frame exp frame)))
-             (if binding
-                 (copy (binding-value binding))
-                 (unbound-var-handler exp frame))))
-          ((pair? exp)
-           (cons (copy (car exp)) (copy (cdr exp))))
-          (else exp)))
-  (copy exp))
-
 (define (query-driver-loop)
   (prompt-for-input input-prompt)
   (let ((q (query-syntax-process (read))))
@@ -54,6 +42,18 @@
                               (contract-question-mark v))))
              (qeval q (singleton-stream '()))))
            (query-driver-loop)))))
+
+(define (instantiate exp frame unbound-var-handler)
+  (define (copy exp)
+    (cond ((var? exp)
+           (let ((binding (binding-in-frame exp frame)))
+             (if binding
+                 (copy (binding-value binding))
+                 (unbound-var-handler exp frame))))
+          ((pair? exp)
+           (cons (copy (car exp)) (copy (cdr exp))))
+          (else exp)))
+  (copy exp))
 
 
 ;;;SECTION 4.4.4.2
@@ -126,8 +126,6 @@
 ;;(put 'lisp-value 'qeval lisp-value)
 
 (define (execute exp)
-;;  (apply (eval (predicate exp) user-initial-environment)
-;;         (args exp)))
   (apply (eval (predicate exp))
 		 (args exp)))
 
