c9fff7a606e5e30374953c038210cb4d0d93410f
uents <uents@ymail.com>
2016-03-27 07:05:02 +0900

    ch5.1: add source code of factrial machine (figure 5.11)

--- ch5-register-simulator/ch5.1.scm
+++ ch5-register-simulator/ch5.1.scm
@@ -25,6 +25,30 @@
  sqrt-done)
 
 
+;;; figure 5.11
+(controller
+   (assign continue (label fact-done))     ; set up final return address
+ fact-loop
+   (test (op =) (reg n) (const 1))
+   (branch (label base-case))
+   ;; Set up for the recursive call by saving n and continue.
+   ;; Set up continue so that the computation will continue
+   ;; at after-fact when the subroutine returns.
+   (save continue)
+   (save n)
+   (assign n (op -) (reg n) (const 1))
+   (assign continue (label after-fact))
+   (goto (label fact-loop))
+ after-fact
+   (restore n)
+   (restore continue)
+   (assign val (op *) (reg n) (reg val))   ; val now contains n * (n-1)!
+   (goto (reg continue))                   ; return to caller
+ base-case
+   (assign val (const 1))                  ; base case: 1!=1
+   (goto (reg continue))                   ; return to caller
+ fact-done)
+
 ;;; ex 5.4
 ;;; - 再帰プロセスは、スタックが必要
 ;;; - 反復プロセスは、末尾再帰ならスタック不要
