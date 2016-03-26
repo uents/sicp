5d8bff2714f92913c078c99eda193e96def9d818
uents <uents@ymail.com>
2016-03-27 07:04:04 +0900

    ch5.2: add stack statistics to register machine simulator

--- ch5-register-simulator/regsim.scm
+++ ch5-register-simulator/regsim.scm
@@ -22,7 +22,9 @@
 		 (stack (make-stack))
 		 (the-instruction-sequence '())
 		 (the-ops (list (list 'initialize-stack
-							  (lambda () (stack 'initialize)))))
+							  (lambda () (stack 'initialize)))
+						(list 'print-stack-statistics
+							  (lambda () (stack 'print-statistics)))))
 		 (register-table (list (list 'pc pc)
 							   (list 'flag flag))))
 	(define (allocate-register name)
@@ -94,18 +96,32 @@
 
 ;;;; stack
 (define (make-stack)
-  (let ((s '()))
+  (let ((s '())
+		(number-pushes 0)
+		(max-depth 0)
+		(current-depth 0))
 	(define (push x)
-	  (set! s (cons x s)))
+	  (set! s (cons x s))
+	  (set! number-pushes (+ 1 number-pushes))
+	  (set! current-depth (+ 1 current-depth))
+	  (set! max-depth (max current-depth max-depth)))
 	(define (pop)
 	  (if (null? s)
 		  (error "[stack] empty stack")
 		  (let ((top (car s)))
 			(set! s (cdr s))
+			(set! current-depth (- current-depth 1))
 			top)))
 	(define (initialize)
 	  (set! s '())
+	  (set! number-pushes 0)
+	  (set! max-depth 0)
+	  (set! current-depth 0)
 	  'done)
+	(define (print-statistics)
+	  (pretty-print (list 'total-pushes '= number-pushes
+						  'max-depth '= max-depth
+						  'curr-depth '= current-depth)))
 
 	;; pushは内部手続きを返すが、
 	;; pop/initializeは内部手続きの実行して結果を返す(ややこしい..)
@@ -113,6 +129,7 @@
 	  (cond ((eq? message 'push) push)
 			((eq? message 'pop) (pop))
 			((eq? message 'initialize) (initialize))
+			((eq? message 'print-statistics) (print-statistics))
 			(else
 			 (error "[stack] unknown request:" + message))))
 	dispatch))
